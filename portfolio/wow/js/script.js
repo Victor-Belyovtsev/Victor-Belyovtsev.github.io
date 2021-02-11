eb5eef4d3a2d347c7cc2275c549d22132a083d20
Victor-Belyovtsev <75089844+Victor-Belyovtsev@users.noreply.github.com>
2021-02-11 19:35:31 +0300

    Add files via upload

---
+++ portfolio/wow/js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        arrows: false,
+        dots: true,
+        slidesToShow: 1,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        infinite: true,
+    });
+  });
